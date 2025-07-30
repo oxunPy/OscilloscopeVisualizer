dd3c652fb0aa151b16f5bdad71adf655e135f829
acer <csharpoxun@gmail.com>
2025-07-31 00:28:24 +0500

    almost done

--- src/main/resources/db/migration/T3__insert_secret_user.sql
+++
@@ -1,3 +0,0 @@
--- #insertSecretUser
-INSERT INTO osci_user(created, status, authset, usertype, info, login, pass)
-VALUES(now(), 'ACTIVE', true, 'SECRET', '---', 'secret', '202CB962AC59075B964B07152D234B70')
--- src/main/resources/db/migration/T4__insert_default_languages.sql
+++
@@ -1,5 +0,0 @@
--- #insertDefaultLanguages
-INSERT INTO osci_language(created, status, name, code)
-VALUES(now(), 'ACTIVE', 'English', 'en'),
-      (now(), 'ACTIVE', 'Uzbek', 'uz'),
-      (now(), 'ACTIVE', 'Русский', 'ru');
